48dcc1ceef382e658984c4e682e1f6ab5d64d678
tarix <tarix@zedat.fu-berlin.de>
2018-06-05 03:36:06 +0200

    - fixes and refactor im import Works: - markets, cryptocurrencies, symbol, history(for one symbol)

--- api/import.py
+++ api/import.py
@@ -1,215 +1,177 @@
-#import from coinapi
+# import from coinapi
 import models
 from api.coinapi_v1 import CoinAPIv1
+from datetime import datetime, date, time
+import dateutil.parser
 
 
-def getSymbol(SymbolID, mark_id=-1,base_cryptocurrency_id=-1, quote_cryptocurrency_id=-1):
-    #result=session.query(models.Cryptocurrency).filter(models.Cryptocurrency.id.in_([id])).all()
-    if (0>1):#(session.query(models.Cryptocurrency).filter(models.Cryptocurrency.id.in_([id])).count()>0):#todo: das hier muss überarbeitet werden; wie geht AND?
-        #return(result[0])
-        print('Fehler')
-    else:
-        #if (int(mark_id)>-1):
-        sym=models.Symbol(id)
-        sym.mark_id=mark_id
-        sym.base_cryptocurrency_id=getCryptocurrencyID("",base_cryptocurrency_id)
-        sym.quote_cryptocurrenc_id=getCryptocurrencyID('',quote_cryptocurrency_id)
+def get_symbol(symbol_id):
+    #TODO: chagne it to findOne
+    result = session.query(models.Symbol).filter(models.Symbol.symbol_global_id == symbol_id).all()
+    if len(result) > 0:
+        return result[0].id
+    return -1
+
+
+def insert_symbol(symbol_id, mark_id=-1, base_cryptocurrency_id=-1, quote_cryptocurrency_id=-1):
+    if session.query(models.Symbol).filter(models.Symbol.symbol_global_id == symbol_id).count() == 0:
+        sym = models.Symbol(id)
+        sym.mark_id = get_market_id_by_name(mark_id)
+        sym.symbol_global_id = symbol_id
+        sym.base_cryptocurrency_id = get_cryptocurrency_id(base_cryptocurrency_id)
+        sym.quote_cryptocurrency_id = get_cryptocurrency_id(quote_cryptocurrency_id)
         session.add(sym)
         session.commit()
-        return (session.query(models.Cryptocurrency).filter(models.Cryptocurrency.id.in_([id])).all()[0])
+        return True
 
-def getCryptocurrencyID(name, name_id):
-    result=session.query(models.Cryptocurrency).filter(models.Cryptocurrency.name_id.in_([name_id])).all()
-    if(session.query(models.Cryptocurrency).filter(models.Cryptocurrency.name_id.like(name_id)).count()>0):
-        return(result[0].id)
-    else:
-        cur=models.Cryptocurrency(name)
-        cur.name_id=name_id
-        #if(int(id)>-1):
-        #cur.id=id
+
+def get_cryptocurrency_id(name_id):
+    # TODO: chagne it to findOne
+    result = session.query(models.Cryptocurrency).filter(models.Cryptocurrency.name_id.in_([name_id])).all()
+    if len(result) > 0:
+        return result[0].id
+    return -1
+
+
+def insert_cryptocurrency(name, name_id):
+    if session.query(models.Cryptocurrency).filter(models.Cryptocurrency.name_id == name_id).count() == 0:
+        cur = models.Cryptocurrency(name)
+        cur.name_id = name_id
         session.add(cur)
         session.commit()
-        return(session.query(models.Cryptocurrency).filter(models.Cryptocurrency.name_id.in_([name_id])).all()[0].id)
-    
-def getMarketID(name, api_url, website,id=-1):
-    result=session.query(models.Mark).filter(models.Mark.name.in_([name])).all()
-    if(session.query(models.Mark).filter(models.Mark.name.in_([name])).count()>0):
-        return(result[0].id)
-    else:
-        mark=models.Mark
-        #if (float(id)>-1):
-        mark.id=id
-        mark=models.Mark(name)
-        mark.api_url=api_url
-        mark.website=website
+        return True
+
+
+def get_market_id_by_name(name):
+    result = session.query(models.Mark).filter(models.Mark.exchange_global_id == name).all()
+    if len(result) > 0:
+        return result[0].id
+    return 0
+
+
+def insert_market(name, api_url, website, id=-1):
+    if session.query(models.Mark).filter(models.Mark.name.in_([name])).count() == 0:
+        mark = models.Mark(name)
+        mark.api_url = api_url
+        mark.website = website
+        mark.exchange_global_id = id
         session.add(mark)
         session.commit()
-        return(session.query(models.Mark).filter(models.Mark.name.in_([name])).all()[0].id)
+        return True
+
+
+def insert_history(ohlcv, symbol):
+    #TODO: do this get_cryptocurrency_id only one time id use symbols
+    # TODO: flush date for the first
+    # now only for BITSTAMP_SPOT_BTC_USD
+    symbol_id = get_symbol(symbol)
+
+    ohlcv_new = models.History()
+    ohlcv_new.start_time_exchange = dateutil.parser.parse(ohlcv['time_period_start'])
+    ohlcv_new.last_time_exchange = dateutil.parser.parse(ohlcv['time_period_end'])
+    ohlcv_new.ask_price = ohlcv['price_open']
+    ohlcv_new.ask_price_last = ohlcv['price_close']
+    ohlcv_new.ask_size = ohlcv['volume_traded']
+    ohlcv_new.ask_price_high = ohlcv['price_high']
+    ohlcv_new.ask_price_low = ohlcv['price_low']
+    ohlcv_new.base_currency_id = get_cryptocurrency_id('BTC')
+    ohlcv_new.quote_currency_id = get_cryptocurrency_id('USD')
+    ohlcv_new.symbol_id = symbol_id
+    #mark_id =
+    session.add(ohlcv_new)
+    session.commit()
+    return True
+
 
 def insertExchange(mark_id, base_cur_id, quote_cur_id, time_exchange, rate):
-    ex=models.Exchange(name)
-    ex.mark_id=mark_id
-    ex.base_cur_id=base_cur_id
-    ex.quote_cur_id=quote_cur_id
-    ex.time_exchange=time_exchange
-    ex.rate=rate
+    ex = models.Exchange(name)
+    ex.mark_id = mark_id
+    ex.base_cur_id = base_cur_id
+    ex.quote_cur_id = quote_cur_id
+    ex.time_exchange = time_exchange
+    ex.rate = rate
     session.add(ex)
 #    session.commit()
 #    return(session.query(models.Exchange).filter(models.Exchange.name_id.in_([name_id])).all()[0].id)
 
 def insertTrade(time, price, size, type, base_cur_id, quote_cur_id, market_id):
-    tr=models.Trade(name)
-    tr.time=time
-    tr.price=price
-    tr.size=size
-    tr.type=type# das ist immer true, da eigentlich unnötig
-    tr.base_cur_id=base_cur_id
-    tr.quote_cur_id=quote_cur_id
-    tr.market_id=market_id
+    tr = models.Trade(name)
+    tr.time = time
+    tr.price = price
+    tr.size = size
+    tr.type = type  # das ist immer true, da eigentlich unnötig
+    tr.base_cur_id = base_cur_id
+    tr.quote_cur_id = quote_cur_id
+    tr.market_id = market_id
     session.add(tr)
-#    session.commit()
+#   session.commit()
 
 def insertTrade(time, price, size, type, base_cur_id, quote_cur_id, market_id):
-    tr=models.Orderbook(name)
-    tr.time=time
-    tr.price=price
-    tr.size=size
-    tr.type=type
-    tr.base_cur_id=base_cur_id
-    tr.quote_cur_id=quote_cur_id
-    tr.market_id=market_id
+    tr = models.Orderbook(name)
+    tr.time = time
+    tr.price = price
+    tr.size = size
+    tr.type = type
+    tr.base_cur_id = base_cur_id
+    tr.quote_cur_id = quote_cur_id
+    tr.market_id = market_id
     session.add(tr)
-#    session.commit()
+#   session.commit()
 
-session=models.Session()
-#basecurID=getCryptocurrencyID('Bitcoin', 'BTC')
-#quotecurID=getCryptocurrencyID('Euro', 'EUR')
-
+session = models.Session()
 api_key = 'E696601D-F684-4237-A4DC-E3BF94A959D2'
 
 api = CoinAPIv1(api_key)
 exchanges = api.metadata_list_exchanges()
 
-importBaseData=1
-    
-if (importBaseData>0):
+importBaseData = 1
+
+if (importBaseData > 0):
     print('Import Exchanges (Markets)')
     for exchange in exchanges:
-        getMarketID(exchange['name'], 'coinapi', exchange['website'], exchange['exchange_id'])
-        
-#        print('Exchange ID: %s' % exchange['exchange_id'])
-#        print('Exchange website: %s' % exchange['website'])
-#        print('Exchange name: %s' % exchange['name'])
+        insert_market(exchange['name'], 'coinapi', exchange['website'], exchange['exchange_id'])
 
     assets = api.metadata_list_assets()
-    print('import Assets (Currencys)')
+    print('import Assets (Currencies)')
     for asset in assets:
-        getCryptocurrencyID(asset['name'],asset['asset_id'])
-    #    print('Asset ID: %s' % asset['asset_id'])
-    #    print('Asset name: %s' % asset['name'])
-    #    print('Asset type (crypto?): %s' % asset['type_is_crypto'])
+        insert_cryptocurrency(asset['name'], asset['asset_id'])
 
     symbols = api.metadata_list_symbols()
     print('import Symbols (Currency-Pairs/Market)')
+    #TODO Batch insert
     for symbol in symbols:
-        getSymbol(symbol['symbol_id'],symbol['exchange_id'],symbol['asset_id_base'],symbol['asset_id_quote'])
-    
-#    print('Symbol ID: %s' % symbol['symbol_id'])
-#    print('Exchange ID: %s' % symbol['exchange_id'])
-#    print('Symbol type: %s' % symbol['symbol_type'])
-#    print('Asset ID base: %s' % symbol['asset_id_base'])
-#    print('Asset ID quote: %s' % symbol['asset_id_quote'])
-#
-#    if (symbol['symbol_type'] == 'FUTURES'):
-#        print('Future delivery time: %s' % symbol['future_delivery_time'])
-#
-#    if (symbol['symbol_type'] == 'OPTION'):
-#        print('Option type is call: %s' % symbol['option_type_is_call'])
-#        print('Option strike price: %s' % symbol['option_strike_price'])
-#        print('Option contract unit: %s' % symbol['option_contract_unit'])
-#        print('Option exercise style: %s' % symbol['option_exercise_style'])
-#        print('Option expiration time: %s' % symbol['option_expiration_time'])
-#
-#Problem: nicht vom Marktplatz abhängig, damit unbrauchbar
-#exchange_rate = api.exchange_rates_get_specific_rate('BTC', 'USD')
-#print('Time: %s' % exchange_rate['time'])
-#print('Base: %s' % exchange_rate['asset_id_base'])
-#print('Quote: %s' % exchange_rate['asset_id_quote'])
-#print('Rate: %s' % exchange_rate['rate'])
-#last_week = datetime.date(2018, 5, 20).isoformat()
-#
-#Problem: s.o.
-#exchange_rate_last_week = api.exchange_rates_get_specific_rate('BTC', 'USD', {'time': last_week})
-#print('Time: %s' % exchange_rate_last_week['time'])
-#print('Base: %s' % exchange_rate_last_week['asset_id_base'])
-#print('Quote: %s' % exchange_rate_last_week['asset_id_quote'])
-#print('Rate: %s' % exchange_rate_last_week['rate'])
-#
-#problem: s.o.
-#current_rates = api.exchange_rates_get_all_current_rates('BTC')
-#
-#print("Asset ID Base: %s" % current_rates['asset_id_base'])
-#for rate in current_rates['rates']:
-#    print('Time: %s' % rate['time'])
-#    print('Quote: %s' % rate['asset_id_quote'])
-#    print('Rate: %s' % rate['rate'])
-#
-#periods = api.ohlcv_list_all_periods()
-#
-#for period in periods:
-#    print('ID: %s' % period['period_id'])
-#    print('Seconds: %s' % period['length_seconds'])
-#    print('Months: %s' % period['length_months'])
-#    print('Unit count: %s' % period['unit_count'])
-#    print('Unit name: %s' % period['unit_name'])
-#    print('Display name: %s' % period['display_name'])
-#
-#ohlcv_latest = api.ohlcv_latest_data('BITSTAMP_SPOT_BTC_USD', {'period_id': '1MIN'})
-#
-#for period in ohlcv_latest:
-#    print('Period start: %s' % period['time_period_start'])
-#    print('Period end: %s' % period['time_period_end'])
-#    print('Time open: %s' % period['time_open'])
-#    print('Time close: %s' % period['time_close'])
-#    print('Price open: %s' % period['price_open'])
-#    print('Price close: %s' % period['price_close'])
-#    print('Price low: %s' % period['price_low'])
-#    print('Price high: %s' % period['price_high'])
-#    print('Volume traded: %s' % period['volume_traded'])
-#    print('Trades count: %s' % period['trades_count'])
-#
-#start_of_2016 = datetime.date(2016, 1, 1).isoformat()
-    marketID=getMarketID('BITSTAMP')
-    print('TODO: einfügen')
-    #ohlcv_historical = api.ohlcv_historical_data('BITSTAMP_SPOT_BTC_USD', {'period_id': '1MIN', 'time_start': start_of_2016})
-#
-#for period in ohlcv_historical:
-#    print('Period start: %s' % period['time_period_start'])
-#    print('Period end: %s' % period['time_period_end'])
-#    print('Time open: %s' % period['time_open'])
-#    print('Time close: %s' % period['time_close'])
-#    print('Price open: %s' % period['price_open'])
-#    print('Price close: %s' % period['price_close'])
-#    print('Price low: %s' % period['price_low'])
-#    print('Price high: %s' % period['price_high'])
-#    print('Volume traded: %s' % period['volume_traded'])
-#    print('Trades count: %s' % period['trades_count'])
-#
+       insert_symbol(symbol['symbol_id'], symbol['exchange_id'], symbol['asset_id_base'], symbol['asset_id_quote'])
 
-    latest_trades = api.trades_latest_data_all()
+       if (symbol['symbol_type'] == 'FUTURES'):
+           print('Future delivery time: %s' % symbol['future_delivery_time'])
 
-    for data in latest_trades:
-        sym=GetSymbol(data['symbol_id'])
-        insertTrade(data['time_exchange'], data['price'], data['size'], true, sym.base_cryptocurrency_id, sym.quote_cryptocurrency_id, sym.mark_id)
-    session.commit()
-##        print('Symbol ID: %s' % data['symbol_id'])
-##        print('Time Exchange: %s' % data['time_exchange'])
-##        print('Time CoinAPI: %s' % data['time_coinapi'])
-##        print('UUID: %s' % data['uuid'])
-##        print('Price: %s' % data['price'])
-##        print('Size: %s' % data['size'])
-##        print('Taker Side: %s' % data['taker_side'])
-##
+       if (symbol['symbol_type'] == 'OPTION'):
+           print('Option type is call: %s' % symbol['option_type_is_call'])
+           print('Option strike price: %s' % symbol['option_strike_price'])
+           print('Option contract unit: %s' % symbol['option_contract_unit'])
+           print('Option exercise style: %s' % symbol['option_exercise_style'])
+           print('Option expiration time: %s' % symbol['option_expiration_time'])
+
+    start_of_2018 = date(2018, 1, 1).isoformat()
+    date_now = date.today().isoformat()
+    marketID = get_market_id_by_name('BITSTAMP')
+    ohlcv_historical = api.ohlcv_historical_data('BITSTAMP_SPOT_BTC_USD', {
+        'period_id': '1DAY',
+        'time_start': start_of_2018,
+        'time_end': date_now,
+        'limit': 10000
+    })
+    for ohlcv in ohlcv_historical:
+        insert_history(ohlcv, 'BITSTAMP_SPOT_BTC_USD')
+
+    # latest_trades = api.trades_latest_data_all()
+    #
+    # for data in latest_trades:
+    #     sym = GetSymbol(data['symbol_id'])
+    #     insertTrade(data['time_exchange'], data['price'], data['size'], true, sym.base_cryptocurrency_id,
+    #                 sym.quote_cryptocurrency_id, sym.mark_id)
+    # session.commit()
+
 ##latest_trades_doge = api.trades_latest_data_symbol('BITTREX_SPOT_BTC_DOGE')
 ##
 ##for data in latest_trades_doge:
@@ -358,5 +320,3 @@ if (importBaseData>0):
 ##
 ##twitter_latest_data = api.twitter_latest_data()
 ##twitter_historical_data = api.twitter_historical_data({'time_start': start_of_2016})
-
-
--- models/symbol.py
+++ models/symbol.py
@@ -1,19 +1,19 @@
-from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, backref
 from .base import Base
 
 
 class Symbol(Base):
-    __tablename__ = 'symbol'
+    __tablename__ = 'symbols'
 
     id = Column(Integer, primary_key=True)
-    mark_id = Column(Integer, ForeignKey('marks.id'), primary_key=True)
-    base_cryptocurrency_id = Column(Integer, ForeignKey('cryptocurrencies.id'), primary_key=True)
-    quote_cryptocurrency_id = Column(Integer, ForeignKey('cryptocurrencies.id'), primary_key=True)
+    mark_id = Column(Integer, ForeignKey('marks.id'))
+    symbol_global_id = Column(String(250), nullable=True)
+    base_cryptocurrency_id = Column(Integer, ForeignKey('cryptocurrencies.id'))
+    quote_cryptocurrency_id = Column(Integer, ForeignKey('cryptocurrencies.id'))
 
     mark = relationship("Mark", backref=backref("mark_cryptocurrency", cascade="all, delete-orphan"))
-    cryptocurrency = relationship("Cryptocurrency", backref=backref("mark_cryptocurrency",
-                                                                    cascade="all, delete-orphan"))
+    #cryptocurrency = relationship("Cryptocurrency", backref=backref("mark_cryptocurrency", cascade="all, delete-orphan"))
 
     def __init__(self,mark_id):
         self.mark_id= mark_id
