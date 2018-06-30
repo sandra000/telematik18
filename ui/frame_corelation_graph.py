93e8158b75d808e63dc1f004a70706a85888db40
tarix <tarix@zedat.fu-berlin.de>
2018-06-30 16:51:25 +0200

    - finish listbox

--- ui/frame_corelation_graph.py
+++ ui/frame_corelation_graph.py
@@ -18,11 +18,22 @@ class CorrelationGraphFrame(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        # set the grid size
+        col = 0
+        while col < 12:
+            self.columnconfigure(col, weight=1)
+            col += 1
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+
         label = tk.Label(self, text="Corelation graph", font=controller.LARGE_FONT)
-        label.pack(pady=5,padx=5)
+        label.grid(row=0, columnspan=12)
+
         self.a = self.figureCorelation.add_subplot(111)
         symbol = SymbolController.Symbol()
         symboldata = symbol.get_all_as_list()
+
         for symbol in symboldata:
             item = SymbolSelect()
             item.var = tk.BooleanVar(self)
@@ -34,36 +45,15 @@ class CorrelationGraphFrame(tk.Frame):
         etherum_name = 3
         zcash_name = 17
 
-        mb = tk.Menubutton(self, text="Select the markets/curecnies", relief=tk.RAISED)
-        mb.pack(side=tk.RIGHT)
-        mb.menu = tk.Menu(mb, tearoff=0)
-        mb["menu"] = mb.menu
-
-        for short_symbol in self.symbol_selected_list:
-            mb.menu.add_checkbutton(label=short_symbol.symbol_global_id, variable=short_symbol.var)
-
-#TODO create separate frame
-        # scrollbar = tk.Scrollbar(self)
-        # scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        #
-        # listbox = tk.Listbox(self)
-        # listbox.pack()
-        #
-        # listbox.config(yscrollcommand=scrollbar.set)
-        # scrollbar.config(command=listbox.yview)
-        #
-        #
-        # for short_symbol in self.symbol_selected_list:
-        #     listbox.insert(tk.END, short_symbol.symbol_global_id)
-
         symbol_list = SymbolList(self, self.symbol_selected_list)
-        symbol_list.pack(side=tk.RIGHT, fill=tk.X)
+        symbol_list.grid(row=1, column=11, sticky=(tk.N, tk.S, tk.E, tk.W))
         symbol_list.config(relief=tk.GROOVE, bd=2)
+        symbol_list.print_selected()
 
-        btn_show_selected = tk.Button(self, text="Print selected", command=self.item_test)
-        btn_show_selected.pack(side=tk.RIGHT)
+        btn_show_selected = tk.Button(self, text="Update", command=symbol_list.print_selected)
+        btn_show_selected.grid(row=2, column=11)
 
-
+#Update graph
         history = HistoryController.History()
         historydata = history.get_all()
         if historydata.values.size == 0:
@@ -83,23 +73,16 @@ class CorrelationGraphFrame(tk.Frame):
         # matplotlib.pyplot.arrow(x, y, dx, dy, hold=None, **kwargs)
 
         canvas = FigureCanvasTkAgg(self.figureCorelation, self)
+#        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        canvas.get_tk_widget().grid(row=1, columnspan=11, sticky=(tk.N, tk.S, tk.E, tk.W))
         canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbarFrame = tk.Frame(master=self)
+        toolbarFrame.grid(row=2, columnspan=11, sticky=tk.W)
+        toolbar = NavigationToolbar2TkAgg(canvas, toolbarFrame)
         toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-
-
-        # dialog_button = tk.Button(self, text="test", command=lambda: self.dialogo())
-        # dialog_button.pack(side='right')
 
         return True
-
-    # def dialogo(self):
-    #     d = MyDialog(self, self.valor, "Select the parameter", "Select")
-    #     self.wait_window(d.top)
 
     def item_test(self):
         for symbol in self.symbol_selected_list:
@@ -132,59 +115,6 @@ class CorrelationGraphFrame(tk.Frame):
         return output_pd
 
 
-# class MyDialog(tk.Frame):
-#     def __init__(self, parent, valor, title, labeltext='', list_of_symbol_var=[]):
-#         self.valor = valor
-#
-#         self.top = tk.Toplevel(parent)
-#         self.top.transient(parent)
-#         self.top.grab_set()
-#         if len(title) > 0: self.top.title(title)
-#         if len(labeltext) == 0: labeltext = 'Valor'
-#         tk.Label(self.top, text=labeltext).pack()
-#         self.top.bind("<Return>", self.ok)
-#         self.e = tk.Entry(self.top, text=valor.get())
-#         self.e.bind("<Return>", self.ok)
-#         self.e.bind("<Escape>", self.cancel)
-#         self.e.pack(padx=15)
-#         self.e.focus_set()
-#         b = tk.Button(self.top, text="OK", command=self.ok)
-#         b.pack(pady=5)
-#
-#
-#         mb = tk.Menubutton(self.top, text="CheckComboBox", relief=tk.RAISED)
-#         mb.pack()
-#         mb.menu = tk.Menu(mb, tearoff=0)
-#         mb["menu"] = mb.menu
-#
-#         self.Item0 = tk.IntVar(self)
-#         self.Item1 = tk.IntVar(self)
-#         self.Item2 = tk.IntVar(self)
-#
-#         mb.menu.add_checkbutton(label="Item0", variable=self.Item0)
-#         mb.menu.add_checkbutton(label="Item1", variable=self.Item1)
-#         mb.menu.add_checkbutton(label="Item2", variable=self.Item2)
-#
-#         button1 = tk.Button(self, text="Item True/False Test", command=self.item_test)
-#         button1.pack()
-#         # for var in list_of_symbol_vars:
-#         #     tk.Checkbutton(self, text="male", variable=var).grid(row=0, sticky=W)
-#
-#     def ok(self, event=None):
-#         print("Has escrito ...", self.e.get())
-#         self.valor.set(self.e.get())
-#         self.top.destroy()
-#
-#
-#     def cancel(self, event=None):
-#         self.top.destroy()
-#
-#     def item_test(self):
-#         print(self.Item0.get())
-#         print(self.Item1.get())
-#         print(self.Item2.get())
-
-
 class SymbolList(tk.Frame):
 
     def __init__(self, parent, symbol_selected_list, picks=[], side=tk.LEFT, anchor=tk.W):
@@ -193,39 +123,14 @@ class SymbolList(tk.Frame):
         scrollbar = tk.Scrollbar(self)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        listbox = tk.Listbox(self, selectmode=tk.MULTIPLE)
-        listbox.pack()
+        self.listbox = tk.Listbox(self, selectmode=tk.MULTIPLE)
+        self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-        listbox.config(yscrollcommand=scrollbar.set)
-        scrollbar.config(command=listbox.yview)
+        self.listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.listbox.yview)
 
         for short_symbol in symbol_selected_list:
-            listbox.insert(tk.END, short_symbol.symbol_global_id)
+            self.listbox.insert(tk.END, short_symbol.symbol_global_id)
 
-
-# class Checkbar(tk.Frame):
-#     # self.selected_symbols = Checkbar(self,
-#     #                                  ['BITSTAMP_SPOT_BTC_USD', 'BITSTAMP_SPOT_ETH_USD', 'BITSTAMP_SPOT_LTC_USD',
-#     #                                   ])
-#     #    self.selected_symbols.pack(side='top', fill='x')
-#     #    self.selected_symbols.config(relief='groove', bd=2)
-#
-#     vars = []
-#
-#     def __init__(self, parent, picks=[], side=tk.LEFT, anchor=tk.W):
-#         tk.Frame.__init__(self, parent)
-#         self.test_var = tk.BooleanVar(self)
-#         for pick in picks:
-#             var = tk.BooleanVar(self)
-#             chk = tk.Checkbutton(self, text=pick, variable=var)
-#             chk.pack(side=side)
-#             self.vars.append(var)
-#         chk = tk.Checkbutton(self, text='test2', variable=self.test_var)
-#         chk.pack()
-#
-#     def state(self):
-#         print("Has escrito ...", self.test_var.get())
-#         return map((lambda var: var.get()), self.vars)
-#
-#     def ok(self, event=None):
-#         print("Has escrito ...", self.test_var.get())
+    def print_selected(self):
+        print(self.listbox.curselection())
