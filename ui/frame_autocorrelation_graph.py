4cd615220a192667b562acb9b9f11ff2c3030c3b
tarix <tarix@zedat.fu-berlin.de>
2018-07-07 17:47:26 +0200

    - add list selection to autocorrelation - fixes

--- ui/frame_autocorrelation_graph.py
+++ ui/frame_autocorrelation_graph.py
@@ -4,6 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from controllers import HistoryController
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from ui.components import SymbolList
 
 # Technical analysts can use autocorrelation to see how much of an impact past prices for a security have on
 #  its future price
@@ -19,40 +20,77 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 class AutocorrelationGraphFrame(tk.Frame):
     figureAutocorrelation = plt.figure()
     valor = tk.StringVar()
+    symbol_selected = []
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+        # Set the grid size
+        col = 0
+        while col < 12:
+            self.columnconfigure(col, weight=1)
+            col += 1
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=4)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=1)
+        self.rowconfigure(4, weight=1)
+
         label = tk.Label(self, text="BTC Autocorrelation graph\n Time unit is 1 day, lagged by 10 days", font=controller.LARGE_FONT)
-        label.pack(pady=5,padx=5)
+        label.grid(row=0, columnspan=12)
         self.a = self.figureAutocorrelation.add_subplot(111)
 
+        self.canvas = FigureCanvasTkAgg(self.figureAutocorrelation, self)
+        self.canvas.get_tk_widget().grid(row=1, rowspan=3, columnspan=10, sticky=(tk.N, tk.S, tk.E, tk.W))
+
+        history = HistoryController.History()
+        self.symbol_data = history.get_all_symbol_from_history()
+        self.symbol_list = SymbolList(self, self.symbol_data)
+        self.symbol_list.grid(row=1, column=10, rowspan=3, columnspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.symbol_list.config(relief=tk.GROOVE, bd=2)
+
+        btn_update_selected = tk.Button(self, text="Update", command=self.renew)
+        btn_update_selected.grid(row=4, column=11)
+
     def on_show(self):
+        history = HistoryController.History()
+        self.symbol_data = history.get_all_symbol_from_history()
+        self.symbol_list = SymbolList(self, self.symbol_data)
+        self.symbol_list.grid(row=1, column=10, rowspan=3, columnspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.symbol_list.config(relief=tk.GROOVE, bd=2)
         self.update()
 
     def update(self):
+        self.a.cla()  # which clears data but not axes
+        symbol_selected = self.symbol_selected
+        history = HistoryController.History()
+        if len(symbol_selected):
+            for item in symbol_selected:
+                current_history_data = history.get_by_symbol_id(item.id)
+                current_prices = current_history_data.ask_price
+                self.a.acorr(current_prices, label=item.symbol_global_id, usevlines=False)
+                self.a.grid(True)
+                self.a.axhline(0, color='black', lw=2)
 
-        #TODO: fix this
-        bitcoin_name = 1
-        etherum_name = 3
-        zcash_name = 17
-        history = HistoryController.History() #object for the databank endpoint
-        historydata = history.get_all() #dataframe
-        if historydata.values.size == 0:
-            return
-        historydata_grouped = historydata.groupby('base_currency_id')
+        else:
+            bitcoin_name = "BITSTAMP_SPOT_BTC_USD"
+            bitcoin_symbol = list(filter(lambda x: x.symbol_global_id == bitcoin_name, self.symbol_data))[0]
+            history_data = history.get_by_symbol_id(bitcoin_symbol.id)
+            current_prices = history_data.ask_price
+            self.a.acorr(current_prices)
+            self.a.grid(True)
+            self.a.axhline(0, color='black', lw=2)
 
+        self.a.legend()
+        self.canvas.draw()
 
-        ask_price = historydata_grouped.get_group(bitcoin_name).ask_price
-        self.a.acorr(ask_price)
-        self.a.grid(True)
-        self.a.axhline(0, color='black', lw=2)
-
-
-        canvas = FigureCanvasTkAgg(self.figureAutocorrelation, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar_frame = tk.Frame(master=self)
+        toolbar_frame.grid(row=4, columnspan=10, sticky=tk.W)
+        toolbar = NavigationToolbar2Tk(self.canvas, toolbar_frame)
         toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         return True
+
+    def renew(self):
+        self.symbol_selected = self.symbol_list.get_selection()
+        self.update()
+
