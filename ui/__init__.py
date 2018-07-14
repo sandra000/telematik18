d8009f9c2d88008122744e482d1d74338271ff03
Holger <Holger>
2018-07-14 16:21:43 +0200

    das hat noch gefehlt

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,6 +1,7 @@
 from .frame_corelation import CorrelationFrame
 from .frame_history_data import HistoryDataFrame
 from .frame_currencies_data import CryptocurrencyDataFrame
+from .frame_neuronal import NeuronalesNetzFrame
 from .frame_market_data import ExchangeDataFrame
 from .frame_symbol_data import SymbolDataFrame
 from .frame_corelation_graph import CorrelationGraphFrame
