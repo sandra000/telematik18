b6449969a646426a28227197a02b704cfb6a8b28
tarix <tarix@zedat.fu-berlin.de>
2018-07-16 21:18:09 +0200

    -  add parameterunterstützung für garch mean

--- ui/frame_garch_analyse_mean.py
+++ ui/frame_garch_analyse_mean.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from matplotlib import pyplot as plt
 from controllers import HistoryController
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from ui.components import SymbolList
+from ui.components import SymbolList, SettingView, ParameterList
 
 
 class GARCHFrameChanging(tk.Frame):
@@ -10,6 +10,7 @@ class GARCHFrameChanging(tk.Frame):
     valor = tk.StringVar()
     test_var = tk.IntVar()
     symbol_selected = []
+    parameter = None
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -19,8 +20,8 @@ class GARCHFrameChanging(tk.Frame):
             self.columnconfigure(col, weight=1)
             col += 1
         self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=4)
-        self.rowconfigure(2, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=2)
         self.rowconfigure(3, weight=1)
         self.rowconfigure(4, weight=1)
 
@@ -33,8 +34,17 @@ class GARCHFrameChanging(tk.Frame):
 
         history = HistoryController.History()
         self.symbol_data = history.get_all_symbol_from_history()
+        self.parameters = history.get_all_parameter_from_history()
+
+        self.setting_view = SettingView(self)
+        self.setting_view.grid(row=1, column=10, sticky=(tk.N, tk.E))
+
+        self.parameter_list = ParameterList(self, self.parameters)
+        self.parameter_list.grid(row=1, column=11, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.parameter_list.config(relief=tk.GROOVE, bd=2)
+
         self.symbol_list = SymbolList(self, self.symbol_data)
-        self.symbol_list.grid(row=1, column=10, rowspan=3, columnspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.symbol_list.grid(row=2, column=10, rowspan=2, columnspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
         self.symbol_list.config(relief=tk.GROOVE, bd=2)
 
         btn_update_selected = tk.Button(self, text="Update", command=self.renew)
@@ -44,7 +54,7 @@ class GARCHFrameChanging(tk.Frame):
         history = HistoryController.History()
         self.symbol_data = history.get_all_symbol_from_history()
         self.symbol_list = SymbolList(self, self.symbol_data)
-        self.symbol_list.grid(row=1, column=10, rowspan=3, columnspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.symbol_list.grid(row=2, column=10, rowspan=2, columnspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
         self.symbol_list.config(relief=tk.GROOVE, bd=2)
         self.update()
 
@@ -55,18 +65,11 @@ class GARCHFrameChanging(tk.Frame):
         if not self.parameter:
             return
         if len(symbol_selected):
+            self.setting_view.update_view(parameter=self.parameter, symbols=symbol_selected)
             for item in symbol_selected:
-                current_history_data = history.get_by_symbol_id_and_parameter_id(item.id, self.parameter)
+                current_history_data = history.get_by_symbol_id_and_parameter_id(item.id, self.parameter.id)
                 current_prices = 100 * current_history_data.ask_price.pct_change(12).dropna()
                 self.a.plot(current_prices, label=item.symbol_global_id)
-        else:
-            bitcoin_name = "BITSTAMP_SPOT_BTC_USD"
-            bitcoin_symbol = list(filter(lambda x: x.symbol_global_id == bitcoin_name, self.symbol_data))[0]
-            history_data = history.get_by_symbol_id(bitcoin_symbol.id)
-            current_prices = 100 * history_data.ask_price.pct_change(12).dropna()
-            if current_prices.size == 0:
-                return
-            self.a.plot(current_prices, color='red', label=bitcoin_name)
 
         self.a.legend()
         self.canvas.draw()
@@ -81,4 +84,9 @@ class GARCHFrameChanging(tk.Frame):
         self.symbol_selected = self.symbol_list.get_selection()
         self.update()
 
-
+    def get_data_for_symbol_list(self, parameter):
+        self.parameter = parameter
+        self.setting_view.update_view(parameter=parameter)
+        history = HistoryController.History()
+        self.symbol_data = history.get_all_symbol_from_history_by_parameter(parameter.id)
+        self.symbol_list.update_list(self.symbol_data)
